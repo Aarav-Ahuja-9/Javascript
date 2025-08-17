ff4882228e33da11057ee65505dd80d9b2b5dc85
Aarav Ahuja <mr.aaravahuja@gmail.com>
2025-08-17 06:28:34 +0000

    commenting all the code

--- 01_Basics/04_Operations.js
+++ 01_Basics/04_Operations.js
@@ -46,14 +46,14 @@ num1 = num2 = num3 = 2 + 4 // Why write this type of messy code this is not a go
 
 // Prefix and Postfix
 
-let x = 3;
-const y = x++;
+// let x = 3;
+// const y = x++;
 
-console.log(`x:${x}, y:${y}`);
+// console.log(`x:${x}, y:${y}`);
 // Expected output: "x:4, y:3"
 
-let a = 3;
-const b = ++a;
+// let a = 3;
+// const b = ++a;
 
-console.log(`a:${a}, b:${b}`);
+// console.log(`a:${a}, b:${b}`);
 // Expected output: "a:4, b:4"
